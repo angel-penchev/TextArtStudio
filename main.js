95f7d975f8e1024ee1afe3bb34e62bec4c4eef11
Ivaylo <ivaylo1231@gmail.com>
2017-03-25 13:16:15 +0200

    Java Script changes: -Added hold down(with the mouse) draw option. -Minor bug fixes.

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-$('td').click(function(e){
+var $mousedown=false
+$('td').mousedown(function(e){
    //alert(e );
    console.log(e);
    e.target.innerHTML='/'
+   $mousedown=true
+});
+$('td').mousemove(function(e){
+   //alert(e );
+   console.log(e);
+   if ($mousedown==true){
+     e.target.innerHTML='/'
+   }
 
+});
+
+$('body').mouseup(function(e){
+  //alert(e );
+  $mousedown=false
+  console.log(e);
 });
