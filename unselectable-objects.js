2770d932f4dd649df3cf8ee4418da780bce77537
Ivaylo <ivaylo1231@gmail.com>
2017-03-25 15:21:57 +0200

    -Uploaded unselectable-objects.js so that the table can not be selected with the mouse. -Added line in editor.html to execute the new script.

---
+++ unselectable-objects.js
@@ -0,0 +1,16 @@
+$.fn.extend({
+  disableSelection: function() {
+    this.each(function() {
+      if (typeof this.onselectstart != 'undefined') {
+        this.onselectstart = function() { return false; };
+      } else if (typeof this.style.MozUserSelect != 'undefined') {
+        this.style.MozUserSelect = 'none';
+      } else {
+        this.onmousedown = function() { return false; };
+      }
+    });
+  }
+});
+$(document).ready(function() {
+  $('label').disableSelection();
+});
